1d2c9b885d0fea9ebdef85edcb942e6e6b61158f
Berserker <luanbraga.lb@gmail.com>
2020-10-16 12:40:29 -0300

    Add files via upload

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -2,8 +2,11 @@ class NegociacaoController {
     private _inputData: HTMLInputElement;
     private _inputQuantidade: HTMLInputElement;
     private _inputValor: HTMLInputElement;
+
+    //cria uma instância de um array do tipo 'Negociacoes'
     private _negociacoes = new Negociacoes();
 
+    //Traz os valores dos campos inseridos no formulario
     constructor(){
         this._inputData = <HTMLInputElement>document.querySelector('#data');
         this._inputQuantidade = <HTMLInputElement>document.querySelector('#quantidade');
@@ -11,16 +14,18 @@ class NegociacaoController {
     }
 
     Adiciona(event: Event){
+
+        //evita que o comportamento padrão do objeto seja disparado.
         event.preventDefault();
 
+        //cria um objeto do tipo 'Negociacao' com os valores trazidos do formulario
         const negociacao = new Negociacao(
             new Date(this._inputData.value.replace(/-/g,',')),
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value)
         );
 
+        //preenche o array de 'Negociacoes' com o objeto do tipo 'Negociacao' 
         this._negociacoes.adiciona(negociacao);
-
-        console.log(this._negociacoes.paraArray());
     }
 }

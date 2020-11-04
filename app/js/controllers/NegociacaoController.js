a2ddf909a2472c24dd0a5649e527fbc64dbd4ead
Berserker <luanbraga.lb@gmail.com>
2020-11-04 20:55:49 -0300

    add NegociacaoController

--- app/js/controllers/NegociacaoController.js
+++ app/js/controllers/NegociacaoController.js
@@ -4,9 +4,20 @@ class NegociacaoController {
         this._inputQuantidade = document.querySelector('#quantidade');
         this._inputValor = document.querySelector('#valor');
     }
+    /* adiciona(event: Event) {
+
+        event.preventDefault();
+
+        const negociacao = new Negociacao(
+            this._inputData.valueAsDate,
+            this._inputQuantidade.valueAsNumber,
+            this._inputValor.valueAsNumber);
+
+        console.log(negociacao);
+    } */
     adiciona(event) {
         event.preventDefault();
-        const negociacao = new Negociacao(this._inputData.value, this._inputQuantidade.value, this._inputValor.value);
+        const negociacao = new Negociacao(new Date(this._inputData.value.replace(/-/g, '/')), parseInt(this._inputQuantidade.value), parseFloat(this._inputValor.value));
         console.log(negociacao);
     }
 }
